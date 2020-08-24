5dbd6c2efbdd976a1c0f9241d3d59a9c3a2bfd37
trinhnk <nguyenkimtrinh1993@gmail.com>
2020-08-24 08:18:01 +0700

    update migration

---
+++ database/migrations/2020_08_23_151629_create_comments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('content', 200);
+            $table->unsignedInteger('article_id');
+            $table->unsignedInteger('user_id');
+            $table->boolean('status');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('article_id')->references('id')->on('articles');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
